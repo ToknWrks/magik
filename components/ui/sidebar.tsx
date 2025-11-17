0deae86db2e9ea89a6c702dce06248ce8440e7f6
ToknWrks <133374682+ToknWrks@users.noreply.github.com>
2025-11-17 12:35:17 -0500

    remove react-flatpicker

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -153,7 +153,7 @@ export default function Sidebar({
                             </SidebarLink>
                           </li>
                           <li className="mb-1 last:mb-0">
-                            <SidebarLink href="/enlightenment">
+                            <SidebarLink href="/real/history">
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
                                 History
                               </span>
@@ -221,19 +221,12 @@ export default function Sidebar({
                             </SidebarLink>
                           </li>
                           <li className="mb-1 last:mb-0">
-                            <SidebarLink href="/enlightenment/5-minute-meditation">
+                            <SidebarLink href="/real/history">
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                5 Minute Meditation
+                                History
                               </span>
                             </SidebarLink>
-                          </li>
-                          <li className="mb-1 last:mb-0">
-                            <SidebarLink href="/dashboard/fintech">
-                              <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                Fintech
-                              </span>
-                            </SidebarLink>
-                          </li>
+                          </li>         
                         </ul>
                       </div>
                     </>

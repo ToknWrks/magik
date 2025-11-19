d99b382d1127d3a2e0d8f577021aef6872b891e7
ToknWrks <133374682+ToknWrks@users.noreply.github.com>
2025-11-18 20:11:35 -0500

    Update sidebar.tsx

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -21,13 +21,6 @@ export default function Sidebar({
   const [breakpoint, setBreakpoint] = useState<string | undefined>(getBreakpoint())
   const expandOnly = !sidebarExpanded && (breakpoint === 'lg' || breakpoint === 'xl')
 
-
-useEffect(() => {
-  if (breakpoint === 'lg' || breakpoint === 'xl') {
-    setSidebarExpanded(false);
-  }
-}, [segments, breakpoint]);
-
   // close on click outside
   useEffect(() => {
     const clickHandler = ({ target }: { target: EventTarget | null }): void => {      

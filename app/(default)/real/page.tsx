7242259c202fc9c0455eda2cf250ea9afaacd808
ToknWrks <133374682+ToknWrks@users.noreply.github.com>
2025-12-04 14:15:28 -0500

    Update page.tsx

--- app/(default)/real/page.tsx
+++ app/(default)/real/page.tsx
@@ -1,5 +1,9 @@
 import IlluminatiArchive from './IlluminatiArchive'; // Adjust path as needed
 
+export const metadata = {
+  title: 'Illuminati - The Real History',
+  description: 'The real history of The Illuminati and mystical traditions through the ages.',
+}
 export default function Page() {
   return <IlluminatiArchive />;
 }

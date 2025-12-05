af80d86020a1a72f3988627f1bb2f236249c633e
ToknWrks <133374682+ToknWrks@users.noreply.github.com>
2025-12-05 10:02:58 -0500

    Update dropdown-profile.tsx

--- components/dropdown-profile.tsx
+++ components/dropdown-profile.tsx
@@ -181,21 +181,7 @@ export default function DropdownProfile({
                   )}
                 </MenuItem>
               </li>
-              <li>
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      className={`flex items-center px-3 py-1.5 ${active ? 'bg-gray-50 dark:bg-gray-700/50' : ''}`}
-                      href="/signup"
-                    >
-                      <svg className="w-4 h-4 mr-2 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
-                      </svg>
-                      Sign Up
-                    </Link>
-                  )}
-                </MenuItem>
-              </li>
+              
             </ul>
           )}
         </MenuItems>

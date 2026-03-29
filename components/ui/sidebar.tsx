bd39f0c39fa0ec01c07546d986cfb8c174a3642b
ToknWrks <133374682+ToknWrks@users.noreply.github.com>
2026-03-29 10:12:28 -0400

    Update sidebar.tsx

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -458,7 +458,7 @@ export default function Sidebar({
                       <path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z" />
                     </svg>
                     <span className="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                      Regen Network
+                      AI Offset 
                     </span>
                   </div>
                 </a>
